1e7dffe8693192415f902a4dc2ef099d573a98f4
Wiktor Szoltysek <wiktorszoltysek@Victors-Personal-MacBook-Pro.local>
2017-11-12 15:50:43 -0500

    Minor TSLint cleanup.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,26 @@
-import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-import { Observable } from 'rxjs/Rx';
-import { of } from 'rxjs/observable/of';
-import { MessageService } from './message.service';
+import {Injectable} from '@angular/core';
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import {Observable} from 'rxjs/Rx';
+import {of} from 'rxjs/observable/of';
+import {MessageService} from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {
+  }
 
   getHero(id: number): Observable<Hero> {
-  // Todo: send the message _after_ fetching the hero
-  this.messageService.add(`HeroService: fetched hero id=${id}`);
-  return of(HEROES.find(hero => hero.id === id));
-}
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 
   getHeroes(): Observable<Hero[]> {
-  	this.messageService.add('HeroService: fetched heroes');
-  	return of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
+
 
 }
