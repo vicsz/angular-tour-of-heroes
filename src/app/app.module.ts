fb60b1a5d0558553d3b0985b01d599c363f6ea9f
Wiktor Szoltysek <wiktorszoltysek@Victors-Personal-MacBook-Pro.local>
2017-11-12 15:31:10 -0500

    Adding dashboard and observable and messaign component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,29 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { AppRoutingModule } from './/app-routing.module';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [HeroService],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Rx';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-  return HEROES;
-}
+  getHeroes(): Observable<Hero[]> {
+  	this.messageService.add('HeroService: fetched heroes');
+  	return of(HEROES);
+  }
 
 }
